ba57f9c58f800bd45bd72d4a098931d078431311
Dimitri <dimitrirestaino@Dimitris-MacBook-Pro.local>
2019-12-29 15:26:01 -0500

    Dimitri - Everything except for gallery pictures and cleanup

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,52 +4,34 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-jQuery(document).ready(function ($) {
+var slideIndex = 1;
+showSlides(slideIndex);
 
-	$('#checkbox').change(function(){
-	  setInterval(function () {
-		  moveRight();
-	  }, 3000);
-	});
-	
-	  var slideCount = $('#slider ul li').length;
-	  var slideWidth = $('#slider ul li').width();
-	  var slideHeight = $('#slider ul li').height();
-	  var sliderUlWidth = slideCount * slideWidth;
-	  
-	  $('#slider').css({ width: slideWidth, height: slideHeight });
-	  
-	  $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-	  
-	  $('#slider ul li:last-child').prependTo('#slider ul');
-  
-	  function moveLeft() {
-		  $('#slider ul').animate({
-			  left: + slideWidth
-		  }, 200, function () {
-			  $('#slider ul li:last-child').prependTo('#slider ul');
-			  $('#slider ul').css('left', '');
-		  });
-	  };
-  
-	  function moveRight() {
-		  $('#slider ul').animate({
-			  left: - slideWidth
-		  }, 200, function () {
-			  $('#slider ul li:first-child').appendTo('#slider ul');
-			  $('#slider ul').css('left', '');
-		  });
-	  };
-  
-	  $('a.control_prev').click(function () {
-		  moveLeft();
-	  });
-  
-	  $('a.control_next').click(function () {
-		  moveRight();
-	  });
-  
-  });    
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
   
       // This example requires the Places library. Include the libraries=places
       // parameter when you first load the API. For example:
